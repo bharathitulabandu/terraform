1175e08d8800f56add8ec413fa3b79159ecfeea4
bharathi tulabandu <61180873+bharathitulabandu@users.noreply.github.com>
2020-03-25 12:41:22 +0530

    Create resouces.tf

---
+++ webserver/resouces.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "webserver" {
+  ami           = "ami-0f90a34c9df977efb "
+  instance_type = "t2.micro"
+  tags = {
+    Name = "single-web server"
+  }
+  user_data = <<-EOF
+            #!/bin/bash
+            yum install httpd
+            service start httpd
+            echo " hello world " > index.html
+            EOF
+}
+
+resource "aws_security_group" "websevrerSG" {
+  name = "single-web server SG"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.webserver.public_ip
+}
